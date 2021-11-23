1a60cfee98942ee9d4473dab353a8d6434932c6f
Sanjog Wanem <37484722+Swanem24@users.noreply.github.com>
2021-11-23 16:32:43 +0000

    Add files via upload

--- Airline Reservation System/src/Booking_System/BookingSys.java
+++ Airline Reservation System/src/Booking_System/BookingSys.java
@@ -3,10 +3,34 @@ package Booking_System;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JInternalFrame;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.JTree;
+import javax.swing.JSpinner;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JEditorPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
 
 public class BookingSys {
 
 	private JFrame frame;
+	private JTextField txtTotalCost;
 
 	/**
 	 * Launch the application.
@@ -36,8 +60,90 @@ public class BookingSys {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(0, 0, 763, 555);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblTitle = new JLabel("Airline Reservation System");
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setBounds(225, 11, 314, 33);
+		frame.getContentPane().add(lblTitle);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 47, 725, 11);
+		frame.getContentPane().add(separator);
+		
+		JLabel lblNewLabel = new JLabel("Ticket Class");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 69, 155, 33);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JComboBox comboBoxTicketClass = new JComboBox();
+		comboBoxTicketClass.setModel(new DefaultComboBoxModel(new String[] {"Economy", "Business", "First"}));
+		comboBoxTicketClass.setBounds(41, 100, 93, 22);
+		frame.getContentPane().add(comboBoxTicketClass);
+		
+		JComboBox comboBoxFrom = new JComboBox();
+		comboBoxFrom.setModel(new DefaultComboBoxModel(new String[] {"London", "Paris", "Berlin", "Tokyo", "Rio", "Denver", "Kathmandu"}));
+		comboBoxFrom.setBounds(41, 177, 93, 22);
+		frame.getContentPane().add(comboBoxFrom);
+		
+		JLabel lblFrom = new JLabel("From");
+		lblFrom.setHorizontalAlignment(SwingConstants.CENTER);
+		lblFrom.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblFrom.setBounds(10, 133, 155, 33);
+		frame.getContentPane().add(lblFrom);
+		
+		JLabel lblTo = new JLabel("To");
+		lblTo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblTo.setBounds(10, 210, 155, 33);
+		frame.getContentPane().add(lblTo);
+		
+		JComboBox comboBoxTo = new JComboBox();
+		comboBoxTo.setModel(new DefaultComboBoxModel(new String[] {"London", "Paris", "Berlin", "Tokyo", "Rio", "Denver", "Kathmandu"}));
+		comboBoxTo.setSelectedIndex(2);
+		comboBoxTo.setBounds(41, 254, 93, 22);
+		frame.getContentPane().add(comboBoxTo);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(144, 101, 30, 20);
+		frame.getContentPane().add(spinner);
+		
+		JButton btnSearchFlights = new JButton("Search Fligths");
+		btnSearchFlights.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				String x = String.valueOf(comboBoxTicketClass.getSelectedItem());
+				System.out.println(x);
+				if(x == "Economy")
+				{
+					txtTotalCost.setText("£100");
+				}
+				else if(x == "Business")
+				{
+					txtTotalCost.setText("£500");
+				}
+				else
+				{
+					txtTotalCost.setText("£1000");
+				}
+			}
+		});
+		btnSearchFlights.setBounds(45, 450, 120, 23);
+		frame.getContentPane().add(btnSearchFlights);
+		
+		JLabel lblNewLabel_1 = new JLabel("Total Cost:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1.setBounds(389, 360, 75, 22);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		txtTotalCost = new JTextField();
+		txtTotalCost.setEditable(false);
+		txtTotalCost.setBounds(467, 362, 86, 20);
+		frame.getContentPane().add(txtTotalCost);
+		txtTotalCost.setColumns(10);
 	}
-
 }
